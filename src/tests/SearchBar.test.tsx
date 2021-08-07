df00e4905536146b9eddbbba6ee921cac371016f
prssanna <prssud@gmail.com>
2021-08-07 16:11:18 +0530

    style: fix formatting

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -1,39 +1,31 @@
-
 import React from 'react';
 import { SearchBar } from 'components/SearchBar';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure, mount } from 'enzyme';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 const searchFn = jest.fn();
 describe('SearchBar', () => {
   it('should not call search function if search box has no value', () => {
     const searchBarComponent = mount(<SearchBar onSearch={searchFn} />);
     const button = searchBarComponent.find('button');
-    expect(button.props()["disabled"]).toBe(true)
-    searchBarComponent
-      .find('button')
-      .simulate('click');
+    expect(button.props()['disabled']).toBe(true);
+    searchBarComponent.find('button').simulate('click');
     expect(searchFn).toHaveBeenCalledTimes(0);
   });
   it('should call search function on button click if search box has value', () => {
     const searchBarComponent = mount(<SearchBar onSearch={searchFn} />);
     const input = searchBarComponent.find('input');
     input.simulate('change', { target: { value: 'Dog' } });
-    searchBarComponent
-      .find('button')
-      .simulate('click');
+    searchBarComponent.find('button').simulate('click');
     expect(searchFn).toHaveBeenCalled();
   });
   it('should call search function on enter if search box has value', () => {
     const searchBarComponent = mount(<SearchBar onSearch={searchFn} />);
     const input = searchBarComponent.find('input');
     input.simulate('change', { target: { value: 'Cat' } });
-    searchBarComponent
-      .find('button')
-      .simulate('keydown', { keyCode: 32 });
+    searchBarComponent.find('button').simulate('keydown', { keyCode: 32 });
     expect(searchFn).toHaveBeenCalled();
   });
-
 });
